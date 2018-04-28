2454b31ddd12f882d25ea153d2d19a069199d52a
bastidiaaz <basty.diaaz@gmail.com>
2018-04-28 20:54:54 +0200

    Adapted index.js to contain the main app. TODO: Make first component

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Navigator,
+  Platform,
+  StyleSheet,
+  Text,
+  UIManager,
+  View
+} from 'react-native';
+import {
+  COLOR,
+  ThemeProvider
+} from 'react-native-material-ui';
 
-AppRegistry.registerComponent('beelive', () => App);
+const uiTheme = {
+    palette: {
+        primaryColor: COLOR.green500,
+    },
+    toolbar: {
+        container: {
+            height: 50,
+        },
+    },
+};
+
+export default class Main extends React.Component {
+    render() {
+        return (
+            <ThemeProvider uiTheme={uiTheme}>
+              <SidePanel  />
+            </ThemeProvider>
+        );
+    }
+}
+
+AppRegistry.registerComponent('beelive', () => Main);
+
+//Activate animations
+UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
