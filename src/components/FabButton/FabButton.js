cfef411e4695fc5c0d1325de97b6ee7533ecdfa1
bastidiaaz <basty.diaaz@gmail.com>
2018-05-10 18:52:49 +0200

    Create FabButton static component

---
+++ src/components/FabButton/FabButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+var styles = StyleSheet.create({
+  fabButton: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#FF5722',
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    alignItems:'center',
+    justifyContent:'center',
+    borderWidth:1,
+    borderColor:'rgba(0,0,0,0.2)'
+  },
+  icon: {
+    fontSize: 20,
+    color: '#FFF'
+  }
+});
+
+class FabButton extends React.Component {
+  render() {
+    return(
+      <TouchableOpacity style={styles.fabButton}>
+        <Icon name="plus" style={styles.icon}/>
+      </TouchableOpacity>
+    )
+  }
+}
+
+export default FabButton;
