262ccc0a67720f205952728a6a627632abc4524e
bastidiaaz <basty.diaaz@gmail.com>
2018-05-12 00:09:38 +0200

    Add icon for elements in list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import {
-  FlatList,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { FlatList, StyleSheet, View } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-// var styles = StyleSheet.create({
-//   container: {
-//
-//   }
-// });
+var styles = StyleSheet.create({
+  iconContainer: {
+    height: 40,
+    width: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 15,
+    backgroundColor: 'black',
+    borderRadius: 30
+  },
+  icon: {
+    fontSize: 22,
+    color: '#FFC107'
+  }
+});
 
 class List extends React.Component {
   constructor(props) {
@@ -22,7 +26,8 @@ class List extends React.Component {
 
   _renderItem = ({item}) => (
     <ListItem
-
+      key={item.key}
+      leftIcon={<View style={styles.iconContainer}><Icon style={styles.icon} name="forumbee"/></View>}
       title={item.name}
       subtitle={`Estado: ${item.status}`}
     />
