51e0d49237b0057cf264d8f5cf8e67592217ad50
bastidiaaz <basty.diaaz@gmail.com>
2018-05-19 16:17:48 +0200

    Merge MainNavigation into index.js

--- index.js
+++ index.js
@@ -5,33 +5,24 @@ import {
   StyleSheet,
   Text,
   UIManager,
-  View,
+  View
 } from 'react-native';
-
-import MainNavigation from './src/components/MainNavigation/MainNavigation';
-
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {screen: 0}
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar backgroundColor="#FFA000" animated={true} />
-        <View style={styles.header}>
-          <Text style={styles.title}>COLMENAS</Text>
-        </View>
-        <MainNavigation />
-      </View>
-    );
-  }
-}
+import { TabNavigator } from 'react-navigation';
+import routes from './src/screens/routes';
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFF'
+  },
+  tab: {
+    backgroundColor: '#FFC107',
+    borderTopColor: '#FFECB3',
+    borderTopWidth: 1
+  },
+  tabIndicator: {
+    backgroundColor: '#FF5722',
+    height: 5
   },
   header: {
     backgroundColor: '#FFC107',
@@ -45,6 +36,41 @@ var styles = StyleSheet.create({
     color: '#FFF'
   }
 });
+
+var options = {
+  tabBarPosition: 'bottom',
+  animationEnabled: true,
+  swipeEnabled: true,
+  lazy: false,
+  tabBarOptions: {
+    showIcon: true,
+    showLabel: false,
+    style: styles.tab,
+    indicatorStyle: styles.tabIndicator
+  }
+};
+
+var TabNav = TabNavigator(routes, options);
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    console.log(this.props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar backgroundColor="#FFA000" animated={true} />
+        <View style={styles.header}>
+          <Text style={styles.title}>COLMENAS</Text>
+        </View>
+        <TabNav />
+      </View>
+    );
+  }
+}
 
 export default Main;
 
