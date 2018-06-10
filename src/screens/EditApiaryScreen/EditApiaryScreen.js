2a0bfa32c5d4ade4f2635f5499a822dd922f1707
bastidiaaz <basty.diaaz@gmail.com>
2018-06-10 15:46:33 +0200

    Create form for EditApiaryScreen and bind data

--- src/screens/EditApiaryScreen/EditApiaryScreen.js
+++ src/screens/EditApiaryScreen/EditApiaryScreen.js
@@ -1,30 +1,44 @@
 import React from 'react';
 import {
-  View,
-  StyleSheet
+  StyleSheet,
+  View
 } from 'react-native';
 import t from 'tcomb-form-native';
+import Button from '../../components/Button/Button';
 import styles from './styles';
 
 const Form = t.form.Form;
 
 const Apiary = t.struct({
-  name: t.String,
-  description: t.String,
-  address: t.String
+  nombre: t.String,
+  descripcion: t.String,
+  direccion: t.String
 });
 
 class EditApiaryScreen extends React.Component {
   constructor(props) {
     super(props);
-  }
+  };
+
 
   render() {
+
+    const defaultValues = {
+      nombre: this.props.apiary.name,
+      descripcion: this.props.apiary.status,
+      direccion: this.props.apiary.population,
+    };
+
     return(
       <View style={styles.container}>
-        <Form type={Apiary} />
+        <View style={{flex: 1}}>
+          <Form
+            value={defaultValues}
+            type={Apiary} />
+        </View>
+        <Button text="GUARDAR APIARIO" />
       </View>
-    )
+        )
   }
 }
 
